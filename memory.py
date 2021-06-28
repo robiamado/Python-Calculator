8e77b707cc9fa0616bb72be103d8c6567c5e7c8f
Roberto Amadori <85162168+robiamado@users.noreply.github.com>
2021-06-28 13:02:53 +0200

    Update memory.py

--- memory.py
+++ memory.py
@@ -4,32 +4,24 @@ class memory_cell():
     def __init__(self, input_str):
         global g_memory_name
         global g_memory_value
-
         try: 
             g_memory_value
         except:
             g_memory_value = ''
             g_memory_name = ''
-
         self.object = {}
         self.values_container = []
         self.values_container_row = []
-
         input_string_handler(input_str) #global variables setter
-
         self.dimension = calc_dimesion(input_str)
-
         if self.dimension == 0:
             self.object[g_memory_name] = g_memory_value
-
         elif self.dimension == 1:
             for input_value in g_memory_value:
                 self.values_container.append(input_value)
             self.object[g_memory_name] = self.values_container
-
         elif self.dimension**2 == len(g_memory_value):
             i = 0
-
             for input_value in g_memory_value:
                 self.values_container_row.append(input_value)
                 if i == (self.dimension-1):
@@ -40,12 +32,11 @@ class memory_cell():
             self.object[g_memory_name] = self.values_container
         else:
             print("\nInvalid input.\n")
-
+            
 # tensor index dimension 0 for scalar, 1 for contravariant vector, 
 # n>1 for any (1,1) tensor (nxn matrix)
 def calc_dimesion(input_string): 
     global g_memory_value
-
     if type(g_memory_value) is float or type(g_memory_value) is bool:
         dimension = 0
     else:
@@ -57,14 +48,11 @@ def calc_dimesion(input_string):
         else:
             dimension = 0
     return dimension
-
 def memory_cleanup(memory_cells):
     global g_memory_value
-
     if g_memory_value == False:
         memory_cells.pop(-1)
         g_memory_value = []
-
     for i in range(len(memory_cells)-1):
         if memory_cells[-1].object.keys() == memory_cells[i].object.keys():
             memory_cells.pop(i)
@@ -79,9 +67,7 @@ def memory_delete(memory_cells):
         for i in range(len(memory_cells)):
             for name,value in memory_cells[i].object.items():
                 print(name + '=' + str(value))
-
         input_str = input('\n>> type a variable to delete: ')
-        
         loop_exit_token = False
         for i in range(len(memory_cells)):
             if loop_exit_token == True:
@@ -107,15 +93,12 @@ def memory_delete(memory_cells):
 def input_string_handler(input_string):
     global g_memory_name
     global g_memory_value
-
     input_string.replace(' ', '')
-    
     if input_string.count('=') == 1:
         equal_sign_pos = input_string.find('=')      
         input_string_name = input_string[:equal_sign_pos]
         input_string_values = input_string[equal_sign_pos+1:]       
         g_memory_name = input_string_name
-
         if input_string_values != '' and (
             input_string_values.count('(') == 1) and (
             input_string_values.count(')') == 1) and (
@@ -127,11 +110,9 @@ def input_string_handler(input_string):
             input_string_values = input_string_values.replace(')','')
             input_string_values = input_string_values.replace('[','')
             input_string_values = input_string_values.replace(']','')
-
             if input_string_values.count(';') == 0: #vector handler
                 input_str_val_rows_split = input_string_values.split(',')
                 input_str_val_split = []
-
                 for i in range(len(input_str_val_rows_split)):
                     try:
                         input_str_val_split.append(
@@ -145,7 +126,6 @@ def input_string_handler(input_string):
                 input_str_val_rows = input_string_values.split(';')
                 input_str_val_rows_split = []
                 input_str_val_split = []
-
                 for i in range(len(input_str_val_rows)):
                     input_str_val_rows_split = input_str_val_rows[i].split(
                     ',')
